d8d310f0c3bb85b78fddafdeb364092f300616a0
Qirat-khalil <167483177+Qirat-khalil@users.noreply.github.com>
2025-03-11 01:20:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,285 @@
+//____________________________________ chapter: 1 
+
+// Q1
+// alert(" Hello User ");
+
+// Q2
+// alert("Error! please enter a valid passsword");
+
+// Q3
+// alert("Welcome to js land \n Happy coding 😊");
+
+// Q4
+// alert("Welcome to js land")
+
+// Q5
+// alert("Hello..I cna run js through my web browser console");
+
+
+//____________________________________ chapter: 2
+
+// Q1
+// let userName;
+// console.log(userName);
+
+// // Q2
+// let myName = "Qirat Khalil"
+// console.log(myName);
+
+// // Q3
+// let message;
+// message = " Hello World"
+// alert(message)
+
+// // Q4
+
+// let userFullName = "Ashar";
+// let age = "15 years old";
+// let qualification = "certified Mobile Application Development";
+// console.log("User Name :" , userFullName);
+// console.log("user age :", age);
+// console.log("Qualification :" , qualification);
+
+// // Q5
+
+// let food = "pizza";
+// alert(food);
+// food = "pizz"
+// alert(food);
+// food = "piz"
+// alert(food);
+// food = "pi"
+// alert(food);
+// food = "p"
+// alert(food);
+
+// // Q6
+
+// let email = "qirat@gmail.com";
+// alert("My email address is "  +  email);
+
+// // Q7
+
+// let book = "A smarter way to learn javascript";
+// alert("I m trying to learn from the book " +  book)
+
+// // Q8
+// document.write("<h1> yah! I can write HTML content through js </h1>");
+
+// // Q9
+// let myString = "▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬";
+// alert(myString);
+
+
+//____________________________________ chapter: 3
+
+// Q1
+
+// let age = 21;
+// alert(`I am ${age} years old`)
+
+// Q2 not done
+
+// // Q3
+// let birthYear = 2003
+// document.write("<p> My birth Year is 2003 </p>")
+// document.write("<p> Data time of my declared Variable is number  </p>")
+
+// // Q4
+
+// let visitorsName = "Jhon Doe"
+// let productTittle = "T-shirt"
+// let quantity = 5
+// console.log("User Name: ", visitorsName ,"ProductName: " , productTittle, "Quantity: ",  quantity );
+
+// document.write("<p> Jhon Doe order 5 T-shirt (s) Minsa store</p>")
+
+
+//____________________________________ chapter: 4
+
+// Q1
+
+// legal variables 
+
+// let $fullName;
+
+// document.write("<p>Variable can contain, number $ and _ forexample $my_name1</p>")
+// document.write("<p>Variable must be begins with a $ or _ or num1</p>")
+// document.write("<p>Variable name are case sensitive</p>")
+// document.write("<p>Variable name shoudls not be js keyword /p>")
+
+// illegl variables
+// document.write("<p>Variable can not contain number or not start with any 1254  or any symbol</p>")
+
+
+
+//____________________________________ chapter: 5
+
+// Q1
+
+// let num1 = 15;
+// let num2 = 5;
+
+// let result = num1 + num2;
+
+// console.log(result);
+
+// document.write( "<p> sum of 5 add 5 is 10 /p>")
+
+// // Q2
+
+// let num3 = 15;
+// let num4 = 5;
+
+// let result1 = num3 - num4;
+// let result2 = num3 * num4;
+// let result3 = num3 / num4;
+// let result4 = num3 % num4;
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+// console.log(result4);
+
+// Q3
+
+// a)
+
+// let num;
+// console.log(num);
+// document.write("<p>Value after declaration is undefined</p>");
+
+// num = 5; 
+// console.log(num);
+// document.write("<p> Intial value is 5 </p>");
+
+// num = num + 1;
+// console.log(num);
+// document.write("<p> Increment value is 6 </p>");
+
+// num = num + 7;
+// console.log(num);
+// document.write("<p> Increment value is 13 </p>");
+
+// num = num - 1;
+// console.log(num);
+// document.write("<p> value after decrement value is 12 </p>");
+
+// num = num % 3;
+// console.log(num);
+// document.write("<p> Reminder is 0</p>");
+
+
+
+// Q4
+
+// let ticket = 600;
+
+// let price = 600 * 5
+// console.log(price);
+// document.write("<p>Total cost of buy 5 tickets to a movie is 3000PKR</p>");
+
+// Q5
+
+// let table = 2;
+
+// console.log(`${table} * 1 = ${table * 1}`);
+// console.log(`${table} * 2 = ${table * 2}`);
+// console.log(`${table} * 3 = ${table * 3}`);
+// console.log(`${table} * 4 = ${table * 4}`);
+// console.log(`${table} * 5 = ${table * 5}`);
+// console.log(`${table} * 6 = ${table * 6}`);
+// console.log(`${table} * 7 = ${table * 7}`);
+// console.log(`${table} * 8 = ${table * 8}`);
+// console.log(`${table} * 9 = ${table * 9}`);
+// console.log(`${table} * 10 = ${table * 10}`);
+
+// Q6  
+
+// let foren = 75;
+// let celi = 24;
+
+// let sumup = ( foren - 32) * 5 / 9;
+// alert(sumup + " " + "C");
+
+// let sumup2 =(celi * 9 / 5) +32
+// alert(sumup2 + " "+ "F");
+
+// Q7
+
+// let store = "shopping store"
+
+// Q8
+
+// let totalMarks = 980;
+// let markObain = 804;
+
+// let sum = 804*100;
+// sum = sum / 980;
+// console.log("percentage " , sum);
+// document.write("<h1>Mark Sheet</h1>")
+// document.write("<p>totalMarks = 980;</p>")
+// document.write("<p>Mark Obtain = 804;</p>")
+// document.write("<p>percentage 82.0408</p>")
+
+
+// Q9
+
+// let dollar = 10;
+// let riyal = 25;
+
+// let calcdollar = dollar * 104.80;
+// let calcriyal = riyal * 28;
+
+// let result6 = calcdollar + calcriyal;
+// console.log(result6);
+// document.write("<h1> Currency in PKR</h1>")
+// document.write("<p>Total Currency is 1748</p>")
+
+
+// Q10
+
+// let number = 5;
+// number = number + 5;
+// console.log(number);
+
+// number = number*10;
+// console.log(number);
+// number = number / 2;
+// console.log(number);
+
+// Q11
+
+
+// let curr = 2025;
+// let birth = 2004;
+
+// alert(curr - birth);
+
+// let userage = prompt("enter your birth year");
+// alert (curr - userage)
+
+// document.write("<h1>Age Calculator</h1>")
+
+// document.write("<p>Current Year : 2025</p>")
+// document.write("<p>Birth Year : 2004</p>")
+// document.write("<p> Your Age is : 22</p>")
+
+// Q12 not done
+
+// Q13 not done
+// document.write("<h1>Life time Supply Calculator</h1>")
+// document.write("<p> Favourite Snacks : Chocolate</p>")
+// document.write("<p> current Age : 22</p>")
+// document.write("<p> Maximum Age : 75</p>")
+// document.write("<p> Amount of snacks per day : 3</p>")
+
+
+// let favsnak = "chocolate"
+// let currage = 20;
+// let maxage = 50;
+// let estimate = 3;
+
+// // let sumall = maxage * estimate  
+// let sumall= 15*65
+// alert(sumall)
+
